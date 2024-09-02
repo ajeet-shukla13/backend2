7aefd3a1f44f408e42e98b13e4bdb88939568eae
Ajeet <ajeetshukla378@gmail.com>
2024-09-03 00:28:04 +0530

    Publishing Backend Project

---
+++ project/src/test/java/com/mysystem/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysystem.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
